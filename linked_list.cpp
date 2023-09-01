2cee1ad4b5605a3a0a125e80082fab922518e9db
Ayesha Siddiqua <128472306+Ayesha-Siddiqua88@users.noreply.github.com>
2023-09-01 21:11:47 +0530

    linked_list.cpp

---
+++ linked_list.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int main(){
+  class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode *fast=head;
+        ListNode *slow=head;
+
+        if(head==NULL|| head->next==NULL){
+            return false;
+        }
+
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+
+            if(fast==slow){
+           return true;
+        }}
+          return false;   
+    }
+}};
